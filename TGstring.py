9c77aca5d8b474125e17e6b0d6929cee451859f4
TheC0ala <76583019+TheC0ala@users.noreply.github.com>
2021-01-10 12:08:51 +0400

    Create TGstring.py

---
+++ TGstring.py
@@ -0,0 +1,23 @@
+MESAJ="TGUSERBOT"
+MESAJ+="\nTelegram: @UserBotTG"
+pkg upgrade
+clear
+echo -e $MESAJ
+echo "Python yüklenir"
+pkg install python -y
+clear
+echo -e $MESAJ
+echo "TeleThon yüklenir"
+pip install telethon
+echo "Requests/BS4 yüklenir"
+pip install requests
+pip install bs4
+clear
+echo -e $MESAJ
+echo "Fayl yazılır"
+curl "https://raw.githubusercontent.com/TheC0ala/TGUserBot/main/TGstring.py" --output "TGstring.py"
+clear
+echo -e $MESAJ
+echo "Qurulum hazırdır, Artıq String ala bilersiz"
+clear
+python TGstring.py
