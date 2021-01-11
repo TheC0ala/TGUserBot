57a7569ab126758206f433156d7134b00c21eba8
TheC0ala <76583019+TheC0ala@users.noreply.github.com>
2021-01-11 17:40:20 +0400

    Update __helpme.py

--- userbot/modules/__helpme.py
+++ userbot/modules/__helpme.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# TGUSERBOT - by TheC0ala
+# TGUSERBOT - by BABAŞ
 
 from userbot import BOT_USERNAME
 from userbot.events import register
