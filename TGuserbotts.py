d04c4b6c244860f499939d67946d624dc6549cf0
TheC0ala <76583019+TheC0ala@users.noreply.github.com>
2021-01-12 12:46:43 +0400

    Update and rename TGuserbotss.py to TGuserbotts.py

--- TGuserbotts.py
+++ TGuserbotts.py
@@ -13,9 +13,9 @@ pip install bs4
 clear
 echo -e $MESAJ
 echo "Fayl yazılır..."
-curl "https://raw.githubusercontent.com/TheC0ala/TGUserBot/main/TGuserbotss.py" --output "TGuserbotss.py"
+curl "https://raw.githubusercontent.com/TheC0ala/TGUserBot/main/TGuserbotts.py" --output "TGuserbotts.py"
 clear
 echo -e $MESAJ
 echo "Qurulum Tamamlandı!, İndi String Ala Bilərsiz."
 clear
-python -m tg_installer.py
+python -m tg_installer
