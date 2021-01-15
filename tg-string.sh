448a4a8b8fba08b871b676ff8f20e9284e4757ab
TheC0ala <76583019+TheC0ala@users.noreply.github.com>
2021-01-15 13:55:43 +0400

    Create tg-string.sh

---
+++ tg-string.sh
@@ -0,0 +1,23 @@
+MESAJ="TGUSERBOT STRING"
+MESAJ+="\nTelegram: @UserBotTG"
+pkg upgrade
+clear
+echo -e $MESAJ
+echo "Python yüklənir..."
+pkg install python -y
+clear
+echo -e $MESAJ
+echo "Telethon yüklənir..."
+pip install telethon
+echo "Requests/BS4 yüklənir..."
+pip install requests
+pip install bs4
+clear
+echo -e $MESAJ
+echo "Fayl yazılır..."
+curl "https://raw.githubusercontent.com/TheC0ala/TGUserBot/main/ddq.py" --output "ddq.py"
+clear
+echo -e $MESAJ
+echo "Qurulum hazırdır! İndi String Session ala bilərsiz"
+clear
+python ddq.py
