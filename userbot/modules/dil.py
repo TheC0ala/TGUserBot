cbcf75bbafca8faca67593a13a6cd3f2678004e7
TheC0ala <76583019+TheC0ala@users.noreply.github.com>
2021-01-17 14:30:24 +0400

    Update dil.py

--- userbot/modules/dil.py
+++ userbot/modules/dil.py
@@ -1,10 +1,10 @@
-# Copyright (C) 2020 Yusuf Usta.
+# Copyright (C) 2020 
 #
 # Licensed under the  GPL-3.0 License;
 # you may not use this file except in compliance with the License.
 #
 
-# Asena UserBot - Yusuf Usta
+# TGUSERBOR - by BABAŞ
 
 from userbot.cmdhelp import CmdHelp
 from userbot import PLUGIN_CHANNEL_ID, CMD_HELP
@@ -21,20 +21,20 @@ async def dil(event):
 
     komut = event.pattern_match.group(1)
     if search(r"y[uü]kle|install", komut):
-        await event.edit("`Dil dosyası yükleniyor... Lütfen bekleyiniz.`")
+        await event.edit("`Dil faylı yüklənir... Zəhmət olmasa gözləyin.`")
         if event.is_reply:
             reply = await event.get_reply_message()
             dosya = await reply.download_media()
 
-            if ((len(reply.file.name.split(".")) >= 2) and (not reply.file.name.split(".")[1] == "asenajson")):
-                return await event.edit("`Lütfen geçerli bir` **AsenaJSON** `dosyası verin!`")
+            if ((len(reply.file.name.split(".")) >= 2) and (not reply.file.name.split(".")[1] == "TGjson")):
+                return await event.edit("`Zəhmət olmasa keçərli` **TGBOTJSON** `faylı verin!`")
 
             try:
                 dosya = loads(open(dosya, "r").read())
             except JSONDecodeError:
-                return await event.edit("`Lütfen geçerli bir` **AsenaJSON** `dosyası verin!`")
+                return await event.edit("`Zəhmət olmasa keçərli` **TGBOTJSON** `faylı verin!`")
 
-            await event.edit(f"`{dosya['LANGUAGE']}` `dili yükleniyor...`")
+            await event.edit(f"`{dosya['LANGUAGE']}` `dili yüklənir...`")
             pchannel = await event.client.get_entity(PLUGIN_CHANNEL_ID)
 
             dosya = await reply.download_media(file="./userbot/language/")
@@ -42,45 +42,45 @@ async def dil(event):
             await reply.forward_to(pchannel)
             
             LANGUAGE_JSON = dosya
-            await event.edit(f"✅ `{dosya['LANGUAGE']}` `dili başarıyla yüklendi!`\n\n**İşlemlerin geçerli olması için botu yeniden başlatın!**")
+            await event.edit(f"✅ `{dosya['LANGUAGE']}` `dili uğurla yükləndi!`\n\n**Əməliyyatların keçərli olması üçün botu yenidən başladın!**")
         else:
-            await event.edit("**Lütfen bir dil dosyasına yanıt verin!**")
+            await event.edit("**Zəhmət olmasa bir dil faylına cavab verin!**")
     elif search(r"bilgi|info", komut):
-        await event.edit("`Dil dosyası bilgileri getiriliyor... Lütfen bekleyiniz.`")
+        await event.edit("`Dil faylının məlumatları gətirilir... Zəhmət olmasa gözləyin.`")
         if event.is_reply:
             reply = await event.get_reply_message()
-            if ((len(reply.file.name.split(".")) >= 1) and (not reply.file.name.split(".")[1] == "asenajson")):
-                return await event.edit("`Lütfen geçerli bir` **AsenaJSON** `dosyası verin!`")
+            if ((len(reply.file.name.split(".")) >= 1) and (not reply.file.name.split(".")[1] == "TGjson")):
+                return await event.edit("`Zəhmət olmasa keçərli` **TGBOTJSON** `faylı verin!`")
 
             dosya = await reply.download_media()
 
             try:
                 dosya = loads(open(dosya, "r").read())
             except JSONDecodeError:
-                return await event.edit("`Lütfen geçerli bir` **AsenaJSON** `dosyası verin!`")
+                return await event.edit("`Zəhmət olmasa keçərli` **TGBOTJSON** `faylı verin!`")
 
             await event.edit(
                 f"**Dil: **`{dosya['LANGUAGE']}`\n"
                 f"**Dil Kodu: **`{dosya['LANGCODE']}`\n"
-                f"**Çevirmen: **`{dosya['AUTHOR']}`\n"
+                f"**Çevirən: **`{dosya['AUTHOR']}`\n"
 
-                f"\n\n`Dil dosyasını yüklemek için` `.dil yükle` `komutunu kullanınız.`"
+                f"\n\n`Dil faylını yükləmək üçün` `.dil yüklə` `komandasını işlədin.`"
             )
         else:
-            await event.edit("**Lütfen bir dil dosyasına yanıt verin!**")
+            await event.edit("**Zəhmət olmasa bir dil faylına cavab verin!**")
     else:
         await event.edit(
             f"**Dil: **`{LANGUAGE_JSON['LANGUAGE']}`\n"
             f"**Dil Kodu: **`{LANGUAGE_JSON['LANGCODE']}`\n"
-            f"**Çevirmen: **`{LANGUAGE_JSON ['AUTHOR']}`\n"
+            f"**Çevirən: **`{LANGUAGE_JSON ['AUTHOR']}`\n"
 
-            f"\n\nDiğer diller için @AsenaDil kanalına bakabilirsiniz."
+            f"\n\nDigər dillər üçün @UserBotTGDil kanalına baxa bilərsiz."
         )
 
 CmdHelp('dil').add_command(
-    'dil', None, 'Yüklediğiniz dil hakkında bilgi verir.'
+    'dil', None, 'Yüklədiyiniz dil faylı haqqında məlumat.'
 ).add_command(
-    'dil bilgi', None, 'Yanıt verdiğiniz dil dosyası hakkında bilgi verir.'
+    'dil melumat', None, 'Cavab verdiyiniz dil faylı haqqında məlumat verər.'
 ).add_command(
-    'dil yükle', None, 'Yanıt verdiğiniz dil dosyasını yükler.'
+    'dil yüklə', None, 'Cavab verdiyiniz dil faylını yükləyər.'
 ).add()
