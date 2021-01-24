8fd63a60615ba94c1320ddd5a6d12a98880138e4
TheC0ala <76583019+TheC0ala@users.noreply.github.com>
2021-01-24 23:59:19 +0400

    Update __ddq.py

--- userbot/modules/__ddq.py
+++ userbot/modules/__ddq.py
@@ -21,9 +21,9 @@ LANG = get_value("__ddq")
 
 # ████████████████████████████████ #
 
-@register(outgoing=True, pattern="^.tgbot(?: |$)(.*)")
+@register(outgoing=True, pattern="^.bot(?: |$)(.*)")
 async def ddq(event):
-    """ .tgbot komanda üçün """
+    """ necesen """
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
