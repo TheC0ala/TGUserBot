b04ec6499eff2c88601c65041c9c00f8558c3b32
TheC0ala <76583019+TheC0ala@users.noreply.github.com>
2021-01-30 13:28:20 +0400

    Create ddqsks.sh

---
+++ ddqsks.sh
@@ -0,0 +1,23 @@
+MESAJ="TGUserBot String"
+MESAJ+="\nTelegram: @UserBotTG"
+pkg upgrade
+clear
+echo -e $MESAJ
+echo "Python yüklənir..."
+pkg install python -y
+clear
+echo -e $MESAJ
+echo "TeleThon yüklənir..."
+pip install telethon
+echo "Requests/BS4 yüklənir..."
+pip install requests
+pip install bs4
+clear
+echo -e $MESAJ
+echo "Fayl yazılır..."
+curl "https://raw.githubusercontent.com/thec0ala/tguserbot/main/ddq.py" --output "ddq.py"
+clear
+echo -e $MESAJ
+echo "Qurulum Bitdi! İndi String Ala Bilərsiz."
+clear
+python ddq.py
