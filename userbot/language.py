b0d70a0a864cf436caf0f65f3806291d1e9bbf1a
TheC0ala <76583019+TheC0ala@users.noreply.github.com>
2021-02-01 21:12:16 +0400

    Add files via upload

--- userbot/language.py
+++ userbot/language.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 
+# Copyright (C) 2020 BABAŞ
 #
 # Licensed under the GPL-3.0 License;
 # you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@ LOGS.info("Dil faylı yüklenir...")
 LANGUAGE_JSON = None
 
 for dil in bot.iter_messages(pchannel, filter=InputMessagesFilterDocument):
-    if ((len(dil.file.name.split(".")) >= 2) and (dil.file.name.split(".")[1] == TGjson")):
+    if ((len(dil.file.name.split(".")) >= 2) and (dil.file.name.split(".")[1] == "TGjson")):
         if path.isfile(f"./userbot/language/{dil.file.name}"):
             try:
                 LANGUAGE_JSON = loads(open(f"./userbot/language/{dil.file.name}", "r").read())
@@ -25,10 +25,10 @@ for dil in bot.iter_messages(pchannel, filter=InputMessagesFilterDocument):
                 remove(f"./userbot/language/{dil.file.name}")
 
                 if path.isfile("./userbot/language/AZ.TGjson"):
-                    LOGS.warn("Varsayılan dil faylı işledilir...")
-                    LANGUAGE_JSON = loads(open(f"./userbot/language/az.TGjson", "r").read())
+                    LOGS.warn("Varsayılan dil faylı işlədilir...")
+                    LANGUAGE_JSON = loads(open(f"./userbot/language/AZ.TGjson", "r").read())
                 else:
-                    raise Exception("Sehv Dil Faylı")
+                    raise Exception("Dil faylı səfdir.")
         else:
             try:
                 DOSYA = dil.download_media(file="./userbot/language/")
@@ -36,10 +36,10 @@ for dil in bot.iter_messages(pchannel, filter=InputMessagesFilterDocument):
             except JSONDecodeError:
                 dil.delete()
                 if path.isfile("./userbot/language/AZ.TGjson"):
-                    LOGS.warn("Varsayılan dil faylı işledilir...")
+                    LOGS.warn("Varsayılan dil faylı işlədilir...")
                     LANGUAGE_JSON = loads(open(f"./userbot/language/AZ.TGjson", "r").read())
                 else:
-                    raise Exception("Dil faylı düzgün deyil.")
+                    raise Exception("Dil faylı səfdir.")
         break
 
 if LANGUAGE_JSON == None:
@@ -47,13 +47,13 @@ if LANGUAGE_JSON == None:
         try:
             LANGUAGE_JSON = loads(open(f"./userbot/language/{LANGUAGE}.TGjson", "r").read())
         except JSONDecodeError:
-            raise Exception("Sehv json faylı")
+            raise Exception("json faylı səfdir.")
     else:
         if path.isfile("./userbot/language/AZ.TGjson"):
-            LOGS.warn("Varsayılan dil faylı işledilir...")
+            LOGS.warn("Varsayılan dil faylı işlədilir...")
             LANGUAGE_JSON = loads(open(f"./userbot/language/AZ.TGjson", "r").read())
         else:
-            raise Exception(f"{LANGUAGE} faylını tapa bilmedim")
+            raise Exception(f"{LANGUAGE} sənədini tapa bilmədim.")
 
 LOGS.info(f"{LANGUAGE_JSON['LANGUAGE']} dili yüklendi.")
 
@@ -61,12 +61,12 @@ def get_value (plugin = None, value = None):
     global LANGUAGE_JSON
 
     if LANGUAGE_JSON == None:
-        raise Exception("Zehmet olmasa evvelce dil faylını yükleyin")
-    else:
+        raise Exception("Please load language file first")
+    else: 
         if not plugin == None or value == None:
             Plugin = LANGUAGE_JSON.get("STRINGS").get(plugin)
             if Plugin == None:
-                raise Exception("Sehv plugin")
+                raise Exception("Invalid plugin")
             else:
                 String = LANGUAGE_JSON.get("STRINGS").get(plugin).get(value)
                 if String == None:
@@ -74,4 +74,4 @@ def get_value (plugin = None, value = None):
                 else:
                     return String
         else:
-            raise Exception("Sehv plugin veya string")
+            raise Exception("Invalid plugin or string")
